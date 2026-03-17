547e2e27a1a121fc371c543aa8c3b92bc2c62ebd
JBlack148 <2683314@dundee.ac.uk>
2026-03-17 16:33:53 +0000

    feat: winner of round

--- Main.py
+++ Main.py
@@ -9,7 +9,15 @@ def createNewTournament():
         playerName = input("What is player " + str(i + 1) + "'s name? ")         
         playerArray.append(playerName)
     gamesArray = generateGameCount(playerArray)
-    print(gamesArray)
+    for i in range(len(gamesArray)):
+        print(gamesArray[i][0] + " vs " + gamesArray[i][1])
+        winner = input("Who won? ")
+        while winner != gamesArray[i][0] and winner != gamesArray[i][1]:
+            winner = input("Please enter a valid player name. Who won? ")
+        if winner == gamesArray[i][0]:
+            exit()
+        elif winner == gamesArray[i][1]:
+            exit()
 
 
 
