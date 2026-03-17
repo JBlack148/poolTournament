0c869f5fb306b99c72713dbd732c565a0b18de38
JBlack148 <2683314@dundee.ac.uk>
2026-03-17 16:51:27 +0000

    feat: basic elo calculation and file comments

--- Main.py
+++ Main.py
@@ -1,26 +1,34 @@
 from roundRobin import *
-    
+from eloCount import *
         
 def createNewTournament():
     playerArray = []
+
+    # get player count
     playerCount = int(input("How many players are playing? "))
-        
+    
+    #populate player array with player names
     for i in range(playerCount):
         playerName = input("What is player " + str(i + 1) + "'s name? ")         
         playerArray.append(playerName)
+
+    # generate games to play and initialize elo for all players
     gamesArray = generateGameCount(playerArray)
+    eloDict = baseElo(playerArray)
+
+    # loop through games and update elo after each game
     for i in range(len(gamesArray)):
         print(gamesArray[i][0] + " vs " + gamesArray[i][1])
         winner = input("Who won? ")
         while winner != gamesArray[i][0] and winner != gamesArray[i][1]:
             winner = input("Please enter a valid player name. Who won? ")
         if winner == gamesArray[i][0]:
-            exit()
+            eloDict = updateElo(gamesArray[i][0], gamesArray[i][1], eloDict)
         elif winner == gamesArray[i][1]:
-            exit()
+            eloDict = updateElo(gamesArray[i][1], gamesArray[i][0], eloDict)
 
 
-
+# main program
 tournamentChoice = input("Create a new tournament or load an existing one? (new/load) " )
 if tournamentChoice == "new":
     createNewTournament()
--- roundRobin.py
+++ roundRobin.py
@@ -1,4 +1,5 @@
 def generateGameCount(players):
+    # generate all possible games for a round robin tournament
     gamesArray = []
     for i in range(len(players)):
         for j in range(len(players)):
