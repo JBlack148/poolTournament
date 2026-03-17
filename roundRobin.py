a394355a5d1c52c783ab45e7171184bce8a0355a
JBlack148 <2683314@dundee.ac.uk>
2026-03-17 17:07:58 +0000

    Revert "Merge pull request #2 from JBlack148/eloCalculation"
    This reverts commit c9cecc4b22f174de196178b7a1c600409314b13b, reversing
    changes made to a9e9bfa0424be6b2257a35b9b9351b79b15d90b6.

--- roundRobin.py
+++
@@ -1,7 +0,0 @@
-def generateGameCount(players):
-    gamesArray = []
-    for i in range(len(players)):
-        for j in range(len(players)):
-            if i != j and (players[j], players[i]) not in gamesArray:
-                gamesArray.append((players[i], players[j]))
-    return gamesArray
